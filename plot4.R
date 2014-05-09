ad1c5a90554f259ba7bcae75cca19ab234eecb27
spcorum <spcorum@gmail.com>
2014-05-09 02:05:47 -0500

    Configrued username and email address.

--- plot4.R
+++ plot4.R
@@ -18,7 +18,7 @@ data$Time <- strptime(paste(data$Date,data$Time), '%Y-%m-%d %H:%M:%S')
 
 # form and save figure 4
 png('plot4.png', width = 480, height = 480)
-par(mfrow = c(2,2), mar = c(2, 4, 2, 1), oma = c(0, 0, 2, 0))
+par(mfrow = c(2,2))
 plot(data$Time, data$Global_active_power, type = 'l', xlab = '', 
      ylab = 'Global Active Power (kilowatts)')
 plot(data$Time, data$Voltage, type = 'l', xlab = 'datetime', 
@@ -27,7 +27,7 @@ plot(data$Time, data$Sub_metering_1, type = 'l', xlab = '',
      ylab = 'Energy sub metering', col = 'black')
 lines(data$Time, data$Sub_metering_2, type = 'l', col = 'red')
 lines(data$Time, data$Sub_metering_3, type = 'l', col = 'blue')
-legend('topright', cex=0.75, box.col =NA, lty = 1, col = c('black', 'red', 'blue'),
+legend('topright', box.col =NA, lty = 1, col = c('black', 'red', 'blue'),
        legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'))
 plot(data$Time, data$Global_reactive_power, type = 'l', xlab = 'datetime', 
      ylab = 'Global_reactive_power')
